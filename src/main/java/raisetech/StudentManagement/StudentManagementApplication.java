2e6be4bab4592eda27be3c7ef63641fe8ee3191c
Mizuki Kobayashi <umizu.51591@gmail.com>
2024-06-18 18:19:04 +0900

    StringUtilsを使った実装

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,20 +1,18 @@
 package raisetech.StudentManagement;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello,World!";
+		String message = "Hhello World";
+
+		//文字列を反転させる
+		String reversedMessage = StringUtils.reverse(message);
+		System.out.println(reversedMessage);
 	}
 
 }
